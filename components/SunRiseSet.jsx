b95bcd56b272e646175c8bf81b69d9055e151d0f
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-01 21:10:42 -0300

    fix: header css, it was causing an overflow on the x scroll

--- components/SunRiseSet.jsx
+++ components/SunRiseSet.jsx
@@ -1,37 +1,33 @@
-import "./SunRiseSet.css"
+import "./SunRiseSet.css";
 
-const SunData = ({data}) => {
-	
-	const timestamp = data
-	const date = new Date(timestamp * 1000)
-	const formattedDate = date.toLocaleTimeString()
+const SunData = ({ data }) => {
+  const timestamp = data;
+  const date = new Date(timestamp * 1000);
+  const formattedDate = date.toLocaleTimeString();
 
-	return (
-		<p className="sunrise-data">{formattedDate}</p>
-	)
-}
+  return <p className="sunrise-data">{formattedDate}</p>;
+};
 
-const SunRiseSet = ({weather}) => {
-	return (
-		<div>
-			{weather && weather.map((data) => (
+const SunRiseSet = ({ weather }) => {
+  return (
+    <div>
+      {weather &&
+        weather.map((data) => (
           <div key={data.city.id}>
-						<div className="sunriseset-container">
-							<div className="sunrise">
-							<p className="sunrise-text">Nascer do Sol</p>
-							<SunData data={data.city.sunrise}/>
-							</div>
-							<div className="sunset">
-							<SunData data={data.city.sunset}/>
-							<p className="sunset-text">Pôr do Sol</p>
-							</div>
-						
-						</div>
-						
-					</div>
-				))}
-		</div>
-		)
-	}
-	
-	export default SunRiseSet
+            <div className="sunriseset-container">
+              <div className="sunrise">
+                <p className="sunrise-text">Nascer do Sol</p>
+                <SunData data={data.city.sunrise} />
+              </div>
+              <div className="sunset">
+                <SunData data={data.city.sunset} />
+                <p className="sunset-text">Pôr do Sol</p>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SunRiseSet;
